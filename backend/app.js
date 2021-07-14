5a106624d512825826c13dffb40424aaaaac2248
ExceedPrince <tamas.bodorkos@hotmail.hu>
2021-07-14 21:24:04 +0200

    some polish on backend

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const allowCors = require('./middleware/allowCors');
+const corsOptions = require('./utils/corsOptions');
+const auth = require('./middleware/auth');
+const notExists = require('./middleware/notExists');
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+app.use(allowCors);
+app.use(cors(corsOptions));
+
+//GET - Basic route
+//Public
+app.get('/', async (req, res) => {
+	res.send('Backend is on')
+});
+
+// Define Routes
+app.use('/api/menu', require('./routes/api/menu'));
+app.use('/api/openings', require('./routes/api/openings'));
+app.use('/api/reservations', require('./routes/api/reservations'));
+
+app.use(notExists);
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,4 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const app = express();
-require('dotenv').config();
-const cors = require('cors');
-const allowCors = require('./middleware/allowCors');
-const corsOptions = require('./utils/corsOptions');
-const auth = require('./middleware/auth');
-const notExists = require('./middleware/notExists');
-
+const app = require('./app');
 const PORT = process.env.PORT || 8080;
-
-const Opening = require('./models/Opening');
-
-// Connect Database
-connectDB();
-
-// Init Middleware
-app.use(express.json());
-app.use(allowCors);
-app.use(cors(corsOptions));
-
-//GET - Basic route
-//Public
-app.get('/', async (req, res) => {
-	res.send('Backend is on')
-});
-
-// Define Routes
-app.use('/api/menu', require('./routes/api/menu'));
-app.use('/api/openings', require('./routes/api/openings'));
-app.use('/api/reservations', require('./routes/api/reservations'));
-
-app.use(notExists);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
